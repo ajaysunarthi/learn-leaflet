9bd0271f6acb933a254f6aded4e4d08f146016fc
ajay <ajaysunarthi@iitj.ac.in>
2016-06-14 23:28:03 +0530

    added assets and time for vacation

---
+++ realTimeGeoLocation/public/js/app.js
@@ -0,0 +1,21 @@
+$(function() {
+
+    var userId = Math.random().toString(16).substring(2, 15);
+    // from stackoverflow
+    var info = $('#infobox');
+    var doc = $(document);
+
+    var tinyIcon = L.Icon.extend({
+		options: {
+			shadowUrl: '../assets/marker-shadow.png',
+			iconSize: [25, 39],
+			iconAnchor:   [12, 36],
+			shadowSize: [41, 41],
+			shadowAnchor: [12, 38],
+			popupAnchor: [0, -30]
+		}
+	});
+	var redIcon = new tinyIcon({ iconUrl: '../assets/marker-red.png' });
+	var yellowIcon = new tinyIcon({ iconUrl: '../assets/marker-yellow.png' });
+
+});
