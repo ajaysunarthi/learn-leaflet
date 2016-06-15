26600b3cca68e531ea9b0827de09e7e19f519471
ajay <ajaysunarthi@iitj.ac.in>
2016-06-15 23:24:43 +0530

    app.js

--- realTimeGeoLocation/public/js/app.js
+++ realTimeGeoLocation/public/js/app.js
@@ -2,6 +2,10 @@ $(function() {
 
     var userId = Math.random().toString(16).substring(2, 15);
     // from stackoverflow
+
+    var socket = io.connect('/');
+	var map;
+
     var info = $('#infobox');
     var doc = $(document);
 
@@ -17,5 +21,118 @@ $(function() {
 	});
 	var redIcon = new tinyIcon({ iconUrl: '../assets/marker-red.png' });
 	var yellowIcon = new tinyIcon({ iconUrl: '../assets/marker-yellow.png' });
+
+
+	var sentData = {};
+
+	var connects = {};
+	var markers = {};
+	var active = false;
+
+	socket.on('load:coords', function(data) {
+		if (!(data.id in connects)) {
+			setMarker(data);
+		}
+
+		connects[data.id] = data;
+			connects[data.id].updated = $.now(); // shothand for (new Date).getTime()
+	});
+
+	// check whether browser supports geolocation api
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(positionSuccess, positionError, { enableHighAccuracy: true });
+	} else {
+		$('.map').text('Your browser is out of fashion, there\'s no geolocation!');
+	}
+
+	function positionSuccess(position) {
+		var lat = position.coords.latitude;
+		var lng = position.coords.longitude;
+		var acr = position.coords.accuracy;
+
+		// mark user's position
+		var userMarker = L.marker([lat, lng], {
+			icon: redIcon
+		});
+		// uncomment for static debug
+		// userMarker = L.marker([51.45, 30.050], { icon: redIcon });
+
+		// load leaflet map
+		map = L.map('map');
+
+		L.tileLayer(
+'http://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
+	maxZoom: 18,
+attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'}
+		).addTo(map);
+
+		// set map bounds
+		map.fitWorld();
+		userMarker.addTo(map);
+		userMarker.bindPopup('<p>You are there! Your ID is ' + userId + '</p>').openPopup();
+
+		var emit = $.now();
+		// send coords on when user is active
+		doc.on('mousemove', function() {
+			active = true;
+
+			sentData = {
+				id: userId,
+				active: active,
+				coords: [{
+					lat: lat,
+					lng: lng,
+					acr: acr
+				}]
+			};
+
+			if ($.now() - emit > 30) {
+				socket.emit('send:coords', sentData);
+				emit = $.now();
+			}
+		});
+	}
+
+	doc.bind('mouseup mouseleave', function() {
+		active = false;
+	});
+
+	// showing markers for connections
+	function setMarker(data) {
+		for (var i = 0; i < data.coords.length; i++) {
+			var marker = L.marker([data.coords[i].lat, data.coords[i].lng], { icon: yellowIcon }).addTo(map);
+			marker.bindPopup('<p>One more external user is here!</p>');
+			markers[data.id] = marker;
+		}
+	}
+
+	// handle geolocation api errors
+	function positionError(error) {
+		var errors = {
+			1: 'Authorization fails', // permission denied
+			2: 'Can\'t detect your location', //position unavailable
+			3: 'Connection timeout' // timeout
+		};
+		showError('Error:' + errors[error.code]);
+	}
+
+	function showError(msg) {
+		info.addClass('error').text(msg);
+
+		doc.click(function() {
+			info.removeClass('error');
+		});
+	}
+
+	// delete inactive users every 15 sec
+	setInterval(function() {
+		for (var ident in connects){
+			if ($.now() - connects[ident].updated > 15000) {
+				delete connects[ident];
+				map.removeLayer(markers[ident]);
+			}
+		}
+	}, 15000);
+
 
 });
